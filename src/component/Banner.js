044d9200c90c0460d9e8616b690e65d8d10d67ca
BeanBeeean <beanbeeean@naver.com>
2023-03-28 21:04:40 +0900

    complete footer section

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -15,7 +15,7 @@ export default function Banner({ movies }) {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 4000,
   };
   return (
     <Slider {...settings}>
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { GoMarkGithub } from "react-icons/go";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  let footerOptions = [
+    "FAQ",
+    "Help Center",
+    "Account",
+    "Media Center",
+    "Investor Relations",
+    "Ways to Watch",
+    "Terms of Use",
+    "Privacy",
+    "Cookies Preferences",
+    "Corporate Infomation",
+    "Contact Us",
+    "Speed Test",
+    "Legal Notices",
+    "Only on Netflix",
+  ];
+  return (
+    <div className="footer-section">
+      <Container>
+        <Row>
+          {footerOptions.map((item) => (
+            <Col className="footer-opt" md={3} sm={6}>
+              {item}
+            </Col>
+          ))}
+        </Row>
+        <div>
+          <a className="footer-link" href="https://www.naver.com">
+            <GoMarkGithub />
+          </a>
+          <a className="footer-link" href="https://www.naver.com">
+            <img src="../image/naverBlog.png" />
+          </a>
+          <span className="footer-email">Email : beanbeeean@naver.com</span>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
